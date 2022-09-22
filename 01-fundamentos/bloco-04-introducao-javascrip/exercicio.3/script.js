0e5c9e9c12b1de9b8e337d7f5384cf8e72b43290
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-22 13:47:14 -0300

    condicao switch e case

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/exercicio.3/script.js
@@ -0,0 +1,9 @@
+const nota = 90;
+
+if (nota >= 80) {
+  console.log("Parabéns, vocé foi aprovada(o)!");
+} else if (nota <= 80 && nota >= 60) {
+  console.log("Voce esta na nossa lista de espera");
+} else {
+  console.log("voce foi reprovada(o)!");
+}
---
+++ 01-fundamentos/bloco-04-introducao-javascrip/exercicio.4/script.js
@@ -0,0 +1,18 @@
+let aluno = "lista";
+
+switch (aluno) {
+  case "aprovado":
+    console.log("Parabéns, você foi aprovada(0)!");
+    break;
+
+  case "lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+
+  case "reprovado":
+    console.log("Você foi reprovado(0)");
+    break;
+
+  default:
+    console.log("Informação incorreta");
+}
