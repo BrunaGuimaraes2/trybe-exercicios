900f9a3a8879da4ed3886d03b56279d0c3e7b64d
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-26 11:57:57 -0300

    adicione soma de tres numeros

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/desafio/maio-tres-numero.js
@@ -0,0 +1,13 @@
+// 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let a = 230;
+let b = 120;
+let c = 49;
+
+if (a > b && a > c) {
+  console.log("O maior numero e A");
+} else if (b > a && b > c) {
+  console.log("O maior numero e B");
+} else {
+  console.log("O maio numero e C");
+}
