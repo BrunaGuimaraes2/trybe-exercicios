79578b7a2de1e39139c8286731a5c8f73147fe9e
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-26 11:49:03 -0300

    adicione uma operaçao que me retorna o numero maio

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/desafio/maio.js
@@ -0,0 +1,10 @@
+// 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+const a = 100;
+const b = 50;
+
+if (a > b) {
+  console.log("O maior numero e A");
+} else {
+  console.log("O maior numero B");
+}
