2ebc5ec645774e92023fad8cd89bec308e03f99a
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-25 21:27:32 -0300

    adicione js e variaves e constantes

--- 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/exercicio.1/exercicio.js
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/exercicio.1/exercicio.js
@@ -1,5 +1,10 @@
 const myName = "Bruna";
-const birthCity = 26;
-let birthYear = 2030;
+const birthCity = "Macapá";
+let birthYear = 2000;
 
+birthYear = 2030;
+birthCity = "Curitiba";
+
+console.log(myName);
 console.log(birthCity);
+console.log(birthYear);
