14f2f0cf0480b18e5c6ec4987eb9ec3e7259664a
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-26 12:06:17 -0300

    adicione estrutura de nomes positivos e negativos

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/desafio/positio.js
@@ -0,0 +1,11 @@
+// 4 - Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let num = 10;
+
+if (num > 0) {
+  console.log("Positivo");
+} else if (num < 0) {
+  console.log("negativo");
+} else {
+  console.log("zero");
+}
