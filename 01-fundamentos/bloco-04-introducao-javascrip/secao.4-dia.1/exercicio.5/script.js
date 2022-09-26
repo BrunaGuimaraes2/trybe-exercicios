4d0f77a12fc95e890dea83f5375056595f42b816
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-25 22:59:42 -0300

    adicione ||

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/exercicio.5/script.js
@@ -0,0 +1,13 @@
+let weekDay = "sabado";
+
+if (
+  weekDay == "segunda-feira" ||
+  "terça-feira" ||
+  "quarta-feira" ||
+  "quinta-feira" ||
+  "sexta-feira"
+) {
+  console.log("Oba, mais um dia de aprendizado na tryba >:D");
+} else {
+  console.log("Finalmente, descanso merecido UwU");
+}
