857f3833f43ba5eecefd23f14914a1a092b4bee9
Bruna Guimaraes <bruna.guimaraes.cunha@gmail.com>
2022-09-26 12:52:37 -0300

    adicione a definição de impar

---
+++ 01-fundamentos/bloco-04-introducao-javascrip/secao.4-dia.1/desafio/number-impar.js
@@ -0,0 +1,13 @@
+// 9 - Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for ímpar.
+// Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+let a = 23;
+let b = 65;
+let c = 97;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
